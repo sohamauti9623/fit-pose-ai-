f6f3853dcec449b7db694fa8372c0eb7ac8013eb
sohamauti9623 <166628211+sohamauti9623@users.noreply.github.com>
2026-04-16 10:04:46 +0530

    Fix Expo runtime issues and navigation dependencies

--- App.tsx
+++ App.tsx
@@ -1,13 +1,14 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import RootNavigation from './navigation';
 
 export default function App() {
   return (
-    <>
+    <GestureHandlerRootView style={{ flex: 1 }}>
       <StatusBar style="light" />
       <RootNavigation />
-    </>
+    </GestureHandlerRootView>
   );
 }
